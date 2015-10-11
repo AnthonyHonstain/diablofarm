73e69e2b12af5a0270b62b9d9c80d2624e21b504
Anthony Honstain <honstain@gmail.com>
2015-10-11 16:10:39 -0700

    Added better formating to tables, added a dumb page for exporting all the data in csv, and cleaned up the forms more bootstrap

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^farm_group/(?P<farm_group_id>[0-9]+)/$', views.farm_group, name='farm_group'),
+    url(r'^farm_group/(?P<farm_group_id>[0-9]+)/export/$', views.farm_group_export, name='farm_group_export'),
     url(r'^farm_group/(?P<farm_group_id>[0-9]+)/farm_event/$', views.farm_event, name='farm_event'),
     url(r'^farm_group/(?P<farm_group_id>[0-9]+)/farm_event/(?P<farm_event_id>[0-9]+)/$', views.farm_event, name='farm_event')
 ]
--- core/views.py
+++ core/views.py
@@ -2,42 +2,78 @@ from django.shortcuts import render, get_object_or_404
 from .models import FarmGroup, FarmEvent
 import datetime
 from django.http.response import HttpResponseRedirect
+from django.http import JsonResponse
 
 from django import forms
 from django.forms import ModelForm
 from django.core.urlresolvers import reverse
-
+from django.core.validators import MinValueValidator
+from django.core import serializers
 
 class FarmEventForm(ModelForm):
+    death_breath_count = forms.IntegerField(validators=[MinValueValidator(0)])
     class Meta:
         model = FarmEvent
-        fields = ['group', 'start_time', 'end_time'] #, 'death_breath_count', 'legendary_count', 'bounty_count', 'blood_shard_count', 'experience_count']
-        widgets = {'group': forms.HiddenInput(),
-                   'start_time': forms.DateTimeInput(attrs={'id':'datetimepicker_start', 'data-date-format':'yyyy-mm-dd hh:ii'}),
+
+        fields = ['start_time', 'end_time', 'death_breath_count'] #, 'death_breath_count', 'legendary_count', 'bounty_count', 'blood_shard_count', 'experience_count']
+        widgets = {'start_time': forms.DateTimeInput(attrs={'id':'datetimepicker_start', 'data-date-format':'yyyy-mm-dd hh:ii'}),
                    'end_time': forms.DateTimeInput(attrs={'id':'datetimepicker_end', 'data-date-format':'yyyy-mm-dd hh:ii'})}
 
 def index(request):
+    '''
+    Root page for the site:
+    * Dumps out a list of the groups you can work on.
+    '''
     farm_groups = FarmGroup.objects.all().order_by('id')
 
     return render(request, 'index.html', {'farm_groups':farm_groups})
 
+def _calculate_time_diff(start_time, end_time):
+    return (datetime.datetime.min + (end_time - start_time)).time()
+
 def farm_group(request, farm_group_id):
+    '''
+    List the most recent events.
+    '''
+    farm_group = get_object_or_404(FarmGroup, pk=farm_group_id)
+    farm_events = FarmEvent.objects.filter(group=farm_group).order_by('start_time')[:10]
+
+    # TODO - FIX DRY CODE
+
+    # This feels super yucky slapping this on the object.
+    for farm_event in farm_events:
+        farm_event.time_diff = _calculate_time_diff(farm_event.start_time, farm_event.end_time)
+
+    return render(request, 'farm_group.html', {'farm_group':farm_group, 'farm_events': farm_events})
+
+def farm_group_export(request, farm_group_id):
+    '''
+    Ghetto dump of all the events in the group.
+    '''
     farm_group = get_object_or_404(FarmGroup, pk=farm_group_id)
     farm_events = FarmEvent.objects.filter(group=farm_group).order_by('start_time')
 
     # This feels super yucky slapping this on the object.
     for farm_event in farm_events:
-        farm_event.time_diff = (datetime.datetime.min + (farm_event.end_time - farm_event.start_time)).time()
-        print(farm_event.time_diff)
+        farm_event.time_diff = _calculate_time_diff(farm_event.start_time, farm_event.end_time)
 
-    return render(request, 'farm_group.html', {'farm_group':farm_group, 'farm_events': farm_events})
+    farm_events_json = serializers.serialize('json', farm_events)
 
+    return render(request, 'farm_group_export.html', {'farm_group':farm_group, 'farm_events_json': farm_events_json})
 
 def farm_event(request, farm_group_id, farm_event_id=None):
+    '''
+    We are taking the group and option event via the url instead of in the form.
+
+    This one endpoint handles the GET, INSERT, and UPDATE for the FarmEvent
+    '''
     farm_group = get_object_or_404(FarmGroup, pk=farm_group_id)
 
     if request.method == 'GET':
-        farm_event = get_object_or_404(FarmEvent, pk=farm_event_id)
+        if (farm_event_id):
+            farm_event = get_object_or_404(FarmEvent, pk=farm_event_id)
+        else:
+            farm_event = None
         form = FarmEventForm(instance=farm_event)
         return render(request, 'farm_event.html', {'farm_group':farm_group, 'farm_event': farm_event, 'form':form})
 
@@ -45,18 +81,18 @@ def farm_event(request, farm_group_id, farm_event_id=None):
         # Update
         if (farm_event_id):
             farm_event = get_object_or_404(FarmEvent, pk=farm_event_id)
-            f = FarmEventForm(request.POST, instance=farm_event)
+            form = FarmEventForm(request.POST, instance=farm_event)
         # Insert
         else:
-            f = FarmEventForm(request.POST)
+            farm_event = FarmEvent()
+            farm_event.group = farm_group
+            form = FarmEventForm(request.POST, instance=farm_event)
 
-        if f.is_valid():
-            f.save()
+        if form.is_valid():
+            form.save()
             return HttpResponseRedirect(reverse('farm_group', kwargs={ 'farm_group_id':farm_group.id }))
         else:
             # TODO - display validation errors.
-            print(f.errors)
+            print(form.errors)
             print(request.POST)
-            f.save()
-
-
+            return render(request, 'farm_event.html', {'farm_group':farm_group, 'farm_event': farm_event, 'form':form})
